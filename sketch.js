af8046bbb51ce7aa0437f33221c3693b107a02f2
wkis172 <150757010+wkis172@users.noreply.github.com>
2024-04-23 17:17:13 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,34 +1,87 @@
-var canvas;
-var sample; // Circle represents Inflation Data Where circle_size is relative to inflation value.
-var sample2; //Square represents Unemployment Data Where square_size is relative to OBS_VALUE.
-var inflationTable;
-var unemploymentTable;
+function draw() {
 
-var HeightCanvasSize = 1; //Adjust Canzas height size relative to your screen size and preference.
-
-function preload() {
-    inflationTable = loadTable('csv/inflation.csv', 'csv', 'header');
-    unemploymentTable = loadTable('csv/unemployment.csv', 'csv', 'header');
-}
-
-function windowResized() {
-    //console.log('resized');
-    resizeCanvas(windowWidth, windowHeight+windowHeight/HeightCanvasSize);
-}
-
-function setup() {
-    canvas = createCanvas(windowWidth, windowHeight+windowHeight/HeightCanvasSize);
-    canvas.position(0,0);  //sets canvas position.
-    canvas.style('z-index', '10'); //draws canvas in background layer.
-    sample = new SampleSystem();
-    sample2 = new SampleSystem2();
-    sample.loadTarget(0,width,0,height);
-    sample2.loadTarget(0,width,0,height);
-    background(255);
-}
-
-function draw(){
-    background(255);
-    sample.drawTargets();
-    sample2.drawTargets();
-}
+    background(245, 0, 76);
+  
+    fill(255,55,155);
+  
+    circle(width/2, height/2, r);
+  
+    fill(55, 155, 255);
+    circle(width/2, height/2, r/2);
+  }
+  
+  
+  /*
+    TASK: 
+    - Can you add a description of the data in this comment 
+      area in a similar way to the other examples?
+    - Can you resize the display so it fits in a smaller screen size?
+  */
+  
+  
+  let icon;
+  let dataset;
+  let unit = 200; //how much each unit will represent in relation to the dataset
+  let margin = 50; //whitespace around edges of the chart
+  let iconSize = 50;
+  
+  
+  //load both image and table in the preload function
+  function preload(){
+    icon = loadImage('person.png');
+    dataset = loadTable('uk-unemployment-00-19.csv', 'header');
+  }
+  
+  function setup() {
+    createCanvas(1050, 950); //width in pixels, height in pixels
+    noStroke();
+  }
+  
+  //draw is run in a loop
+  function draw() {
+    //draw a white background
+    background(255, 254, 252);
+    
+    //header
+    fill(255, 0, 0);
+    textFont("avenir");
+    textAlign(RIGHT);
+    textSize(40);
+    let header = "Unemployment\n in Great Britain\n 2007-2019";
+    text(header, width-margin, margin);
+  
+    image(icon, width-margin-iconSize, 220, iconSize, iconSize);
+    textSize(15);
+    fill(0);
+    text("200,000 people", width-margin, 270);
+    
+    //source
+    textSize(15);
+    text("Source: ONS", width-35, height-5);
+    
+    //loop through each row
+    for(let i=0; i<dataset.getRowCount(); i++){
+      let unemployment = round(dataset.getNum(i, 1)/unit);
+      
+      //set fill colour to red again
+      fill(255, 0, 0);
+      
+      //add year label
+      textSize(15);
+      textAlign(CENTER);
+      let samplePeriod = dataset.getString(i, 0);
+      text(samplePeriod, margin, margin+i*(iconSize+20));
+      
+      //loop to draw each unit in vertical column
+      imageMode(CENTER);
+      for(let j=0; j<unemployment; j++){
+        fill(0);
+        let y = margin+i*(iconSize+20);
+        let x = margin+iconSize+j*(iconSize/2+10);
+        image(icon, x, y, iconSize, iconSize);
+      }
+      
+    }
+    
+  }
+  
