2028b65c100f7f59dbe8e5bdf4c0c87283163ca9
panosleontsinis <123083154+panosleontsinis@users.noreply.github.com>
2024-04-24 11:22:13 +0100

    Update chart1.js

--- chart1.js
+++ chart1.js
@@ -1,33 +1,23 @@
 
+const labels = Utils.months({count: 7});
 const data = {
-    labels: ['United States', 'China', 'Japan', 'Brazil', 'France', 'India'],
-    datasets: [{
-        label: 'Inflation rate Amongst Global Economies',
-        backgroundColor: 'rgba(255, 99, 132, 0.2)', // Red
-        borderColor: 'rgba(255, 99, 132, 1)',
-        borderWidth: 1,
-        data: [2.8, 1, 2.2, 4.1, 2.4, 4.6]
-    }]
+  labels: labels,
+  datasets: [{
+    label: 'My First Ddtaset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  }]
 };
-
-
-const options = {
-    scales: {
-        yAxes: [{
-            ticks: {
-                beginAtZero: true
-            }
-        }]
-    }
+const labels = Utils.months({count: 7});
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Ddtaset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  }]
 };
-
-
-const ctx = document.getElementById('myChart').getContext('2d');
-
-
-const myChart = new Chart(ctx, {
-    type: 'line',
-    data: data,
-    options: options
-});
-
